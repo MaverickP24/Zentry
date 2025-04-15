cbf3bc81170cf164f34fe29ac601ee575c18b21f
) <pratyushparida36@gmail.com>
2025-04-16 04:06:46 +0530

    footer and contact page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import Hero from './components/Hero'
 import About from './components/About'
+import NavBar from './components/Navbar'
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <>
     <main className='overflow-x-hidden relative min-h-screen w-screen'>
+      <NavBar/>
       <Hero/>
       <About/>
+      
+      <Contact />
+      <Footer />
     </main>
     </>
   )
